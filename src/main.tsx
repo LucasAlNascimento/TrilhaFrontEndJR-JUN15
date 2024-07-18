8d4c885db5b3d7ed729f86488112926250450d38
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-07-18 17:14:19 -0300

    💄 feat: Estilização do componente Home
    Inclusão de imagem e textos informativos (objetivo de carreira, sobre e informações pessoais)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/Home/index.tsx'
+import './styles/animations.scss'
+import './styles/styles.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
