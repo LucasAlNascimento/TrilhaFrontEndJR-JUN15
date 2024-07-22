b9ded96773f4baa88da113589909b1e57d54467e
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-07-22 11:12:30 -0300

    💄 feat: Estilização do componente Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,15 @@
+import './styles.scss'
+
 export default function Footer() {
-    return(
-        <>
-        </>
+    return (
+        <footer className='footer'>
+            <div className='footer-info-container'>
+                <div className='footer-info'>
+                    <span>Powered by</span>
+                    <span>Código Certo</span>
+                </div>
+                <img src='/codigocertologo.webp' alt='Código Certo Logo' />
+            </div>
+        </footer>
     )
 }
